7548336e674ca6c08a798c34e4e160040ce18919
Rubén Pacheco <ruben.mtz.pacheco@gmail.com>
2019-04-18 11:00:16 +0200

    Update IOT.R

--- IOT.R
+++ IOT.R
@@ -1,8 +1,8 @@
 library(jsonlite)
 
 #Setting Working directory for files
-setwd("C:\Users\PC\Documents\BABD - Projects, Hands On\IOT\ligthing-optimization-r")
-
+#setwd("C:\Users\PC\Documents\BABD - Projects, Hands On\IOT\ligthing-optimization-r")
+setwd("~/Documents/GitHub/Ligthing_Optimization_Problem_In_R")
 #Reading files and cleansing
 #JSON File with forecast
 forecast <- fromJSON("forecast.json")
@@ -85,7 +85,71 @@ barplot(agg_movement$x, main="Light", xlab="Hour",
 # Forecasted_lux per hour
 #############################################################
 
-#TODO: Codigo ruben
+#Create dataframe to be used in function containing forecasted_lux, hour and day
+#Convert hour to numeric
+light$Hour <- as.numeric(light$Hour)
+forecast$Hour <- as.numeric(forecast$Hour)
+forecast$clouds <- as.numeric(forecast$clouds$all)
+forecast$Hour[forecast$Hour == 6 ] <- 7
+
+#Filter data to include weekdays and work hours
+light <- subset(light, light$Hour >= 7 & light$Hour <= 20 & (light$Day != "Saturday" & light$Day != "Sunday"), select = c(value,Hour,Day))
+forecast <- subset(forecast, forecast$Hour >= 7 & forecast$Hour <= 20, select = c(clouds,Hour,Day))
+
+#Fill values to complete hours and weekdays
+Hour <- c(8,10,11,13,14,16,17,19,20)
+clouds <- c(0,8,8,44,44,68,68,48,48)
+Day <- c("Monday","Monday","Monday","Monday","Monday","Monday","Monday","Monday","Monday")
+forecast_complete <- data.frame(clouds,Hour,Day)
+clouds <- c(56,68,68,76,76,76,76,80,80)
+Day <- c("Tuesday","Tuesday","Tuesday","Tuesday","Tuesday","Tuesday","Tuesday","Tuesday","Tuesday")
+forecast_complete <- rbind(forecast_complete,data.frame(clouds, Hour, Day))
+clouds <- c(36,68,68,76,76,92,92,100,100)
+Day <- c("Wednesday","Wednesday","Wednesday","Wednesday","Wednesday","Wednesday","Wednesday","Wednesday","Wednesday")
+forecast_complete <- rbind(forecast_complete,data.frame(clouds, Hour, Day))
+clouds <- c(92,92,92,92,92,92,92,92,92)
+Day <- c("Thursday","Thursday","Thursday","Thursday","Thursday","Thursday","Thursday","Thursday","Thursday")
+forecast_complete <- rbind(forecast_complete,data.frame(clouds, Hour, Day))
+clouds <- c(12,56,56,80,80,56,56,80,80)
+Day <- c("Friday","Friday","Friday","Friday","Friday","Friday","Friday","Friday","Friday")
+forecast_complete <- rbind(forecast_complete,data.frame(clouds, Hour, Day))
+forecast_final <- rbind(forecast_complete,forecast)
+
+agg_light = aggregate(light$value,by = list(light$Hour,light$Day),FUN = mean)
+names(agg_light)[names(agg_light) == "Group.1"] <- "Hour"
+names(agg_light)[names(agg_light) == "Group.2"] <- "Day"
+names(agg_light)[names(agg_light) == "x"] <- "light"
+
+#Merge forecast and Light distribution
+Lux <- merge(forecast_final,agg_light, by=c("Hour","Day"))
+#Calculate Forecasted lux
+Lux$Forecasted <- (1 - (Lux$clouds*0.01) * 0.40) * Lux$light
+#Select relevant variables and change days and hours for loop function
+Forecasted_Lux <- Lux[, c("Forecasted","Hour", "Day")]
+Forecasted_Lux$Day <- as.character(Forecasted_Lux$Day)
+Forecasted_Lux$Day[Forecasted_Lux$Day == "Monday" ] <- "1"
+Forecasted_Lux$Day[Forecasted_Lux$Day == "Tuesday" ] <- "2"
+Forecasted_Lux$Day[Forecasted_Lux$Day == "Wednesday" ] <- "3"
+Forecasted_Lux$Day[Forecasted_Lux$Day == "Thursday" ] <- "4"
+Forecasted_Lux$Day[Forecasted_Lux$Day == "Friday" ] <- "5"
+Forecasted_Lux$Day <- as.numeric(Forecasted_Lux$Day)
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 7] <- 1
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 8] <- 2
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 9] <- 3
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 10] <- 4
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 11] <- 5
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 12] <- 6
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 13] <- 7
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 14] <- 8
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 15] <- 9
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 16] <- 10
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 17] <- 11
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 18] <- 12
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 19] <- 13
+Forecasted_Lux$Hour[Forecasted_Lux$Hour == 20] <- 14
+names(Forecasted_Lux)[names(Forecasted_Lux) == "Forecasted"] <- "forecasted_lux"
+names(Forecasted_Lux)[names(Forecasted_Lux) == "Day"] <- "day"
+names(Forecasted_Lux)[names(Forecasted_Lux) == "Hour"] <- "hour"
 
 ##############################################################
 # Optimization Code
